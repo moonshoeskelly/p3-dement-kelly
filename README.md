ffd23d3f4697fa9d8ddb4ed58fd547a3f1e84adf
Kelly DeMent <kdement@ufl.edu>
2019-11-23 21:50:53 -0500

    Code second layer

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # p3-dement-kelly
+
+Bob up and down animation: https://codepen.io/ktrane331/pen/dzaPZz
--- src/main.js
+++ src/main.js
@@ -9,37 +9,97 @@ const layerThree = document.getElementById('layer-three');
 
 
 //Our Functions
+
+//First Layer loop
 function loopThrough () {
 
-  const changeColor = () => this.classList.toggle('red');
-  const changeColorTwo = () => this.classList.toggle('blue');
-  for (let i=0; i < this.textContent.length * 2; i++) {
-    if ([0, 1, 4, 5, 8, 9, 12, 13].indexOf(i) > -1) {
-      setTimeout(changeColor, i * 1000);
-    } else {
-      setTimeout(changeColorTwo, i * 1000);
-    }
+  const changeColor = () => this.classList.toggle('white');
+  for (let i=0; i < this.firstElementChild.textContent.length * 2; i++) {
+      setTimeout(changeColor, i * 750);
   }
 };
 
-function loopThroughNumber () {
-  const changeColor = () => this.classList.toggle('red');
-  const numStringValue = this.textContent;
-  for (let i=0; i < Number(numstringValue * 2); i++) {
-      setTimeout(changeColor, i * 1000);
-    }
-};
+// function loopThrough () {
+//
+//   const changeColor = () => this.classList.toggle('white');
+//   const changeColorTwo = () => this.classList.toggle('blue');
+//   for (let i=0; i < this.firstElementChild.textContent.length * 2; i++) {
+//     if ([0, 1, 4, 5, 8, 9, 12, 13].indexOf(i) > -1) {
+//       setTimeout(changeColor, i * 1000);
+//     } else {
+//       setTimeout(changeColorTwo, i * 1000);
+//     }
+//   }
+// };
 
+//First Layer Changes to Second Layer
 function changeLayer () {
   const swap = () => {
     layerOne.classList.add('hide');
     layerTwo.classList.remove('hide');
   }
-  setTimeout(swap, this.textContent.length * 2 * 1000);
+  setTimeout(swap, this.firstElementChild.textContent.length * 2 * 750);
 };
 
-//Event Listener
+//First Layer Event Listener
 for (let i=0; i < myButtons.length; i++) {
   myButtons[i].addEventListener('click', loopThrough);
   myButtons[i].addEventListener('click', changeLayer);
+};
+
+
+
+
+
+//Second Layer Loop
+
+function loopThroughNumber () {
+  const changeColor2 = () => this.classList.toggle('bob');
+  const numStringValue = this.firstElementChild.textContent;
+  for (let i=0; i < Number(numStringValue) * 2; i++) {
+      setTimeout(changeColor2, i * 750);
+    }
+};
+
+//Second Layer Changes to Third Layer
+function changeLayerTwo () {
+  const swap = () => {
+    layerTwo.classList.add('hide');
+    layerThree.classList.remove('hide');
+  }
+  setTimeout(swap, Number(this.firstElementChild.textContent) * 2 * 750);
+};
+
+//Second Layer Event Listener
+for (let i=0; i < mySecondButtons.length; i++) {
+  mySecondButtons[i].addEventListener('click', loopThroughNumber);
+  mySecondButtons[i].addEventListener('click', changeLayerTwo);
+};
+
+
+
+
+//Third Layer Loop
+
+function loopThroughNumber () {
+  const changeColor2 = () => this.classList.toggle('blue');
+  const numStringValue = this.firstElementChild.textContent;
+  for (let i=0; i < Number(numStringValue) * 2; i++) {
+      setTimeout(changeColor2, i * 750);
+    }
+};
+
+//Second Layer Changes to Third Layer
+function changeLayerTwo () {
+  const swap = () => {
+    layerTwo.classList.add('hide');
+    layerThree.classList.remove('hide');
+  }
+  setTimeout(swap, Number(this.firstElementChild.textContent) * 2 * 750);
+};
+
+//Second Layer Event Listener
+for (let i=0; i < mySecondButtons.length; i++) {
+  mySecondButtons[i].addEventListener('click', loopThroughNumber);
+  mySecondButtons[i].addEventListener('click', changeLayerTwo);
 };
